8c7a14820e7f859d9d93c11395b1a1d430512c60
Mark <30897048+HoChiak@users.noreply.github.com>
2019-09-27 17:56:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="WhyPy",
     version="0.0.7",
     author="HoChiak",
     author_email="mark.henss@gmx.de",
     description="Toolbox for Causal Inference with Additive Noise Models",
-    long_description=long_description,
+    long_description="See [GitHub](https://github.com/HoChiak/whypy) for long description",
     long_description_content_type="text/markdown",
     url="https://github.com/HoChiak/whypy",
     packages=setuptools.find_packages(),
